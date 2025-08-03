2e725d1dc922da45f4ab8d5ab995105f0e20abe1
rohanade37 <rohanade37@gmail.com>
2025-08-03 10:08:35 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -10,15 +10,15 @@ dotenv.config({
 
 
 connectDB()
-
-
-
-
-
-
-
-
-
+.then (() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is runing at port : ${process.env.PORT}`);
+      
+    })
+})
+.catch((err) => {
+    console.log("Mongo db connection faield !!!", err);
+})
 
 
 
@@ -62,6 +62,12 @@ connectDB()
 //       throw err
 //  }
 // }) ()
+
+
+
+
+
+
 
 
 
